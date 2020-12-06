db4a6ad7e2f71e5371ce66153be10a3b2042991b
Roshni Prajapati <45714224+tinycosmos@users.noreply.github.com>
2020-12-06 20:56:24 +0530

    Add files via upload

--- animate.js
+++ animate.js
@@ -1,16 +1,29 @@
+var s = "is Light!⚡"; 
+
 document.getElementById("name").addEventListener("click", function myfunction () {
     var name = document.getElementById("name");
-
-        if (name.innerHTML === "Roshni?") {
-            name.innerHTML = "means Light!💡";
-            name.classList.remove("name");
-            name.classList.add("name-toggle");
-        }
-        else if (name.innerHTML === "means Light!💡") {
-            name.innerHTML = "Roshni?";
-            name.classList.remove("name-toggle");
-            name.classList.add("name");
-        }
+        
+    if (name.innerHTML === "Roshni?") {
+        name.innerHTML = s;
+        name.classList.remove("name");
+        name.classList.add("name-toggle");
+    }
+    else if (name.innerHTML === s) {
+        name.innerHTML = "Roshni?";
+        name.classList.remove("name-toggle");
+        name.classList.add("name");
+    }
 
     }
 )
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
