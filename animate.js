ae3c8ad04e8d31de2ed09e8771e0639fdb0edb69
Roshni Prajapati <45714224+tinycosmos@users.noreply.github.com>
2020-12-12 15:38:39 +0530

    Add files via upload

--- animate.js
+++ animate.js
@@ -13,17 +13,38 @@ document.getElementById("name").addEventListener("click", function myfunction ()
         name.classList.remove("name-toggle");
         name.classList.add("name");
     }
-
     }
 )
 
+///////////
 var myVar;
-
 function myFunction() {
-  myVar = setTimeout(showPage, 3000);
+  myVar = setTimeout(showPage, 1000);
 }
-
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+
+///////////
+
+function setClipboard(value) {
+  var tempInput = document.createElement("input");
+  tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+  tempInput.value = value;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+
+  var tooltip = document.getElementById("myTooltip");
+tooltip.innerHTML = "Mail Copied!";
+}
+
+
+function outFunc() {
+var tooltip = document.getElementById("myTooltip");
+tooltip.innerHTML = "Copy to clipboard";
+}
+
