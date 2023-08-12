7cfd2a8cd2caea070a77b65dc724e5646799cab1
Roshni Prajapati <45714224+roshnidesigns@users.noreply.github.com>
2023-08-12 23:50:39 +0530

    Add files via upload

--- animate.js
+++ animate.js
@@ -1,88 +1,66 @@
-$(document).ready(function(){
-    $('.menu-toggle').click(function () {
-        $('nav').toggleClass('active')
-    })
+$(document).ready(function () {
+  $('.menu-toggle').click(function () {
+    $('nav').toggleClass('active')
+  })
 })
 
-function setClipboard(value) {
-  var tempInput = document.createElement("input");
-  tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-  tempInput.value = value;
-  document.body.appendChild(tempInput);
-  tempInput.select();
-  document.execCommand("copy");
-  document.body.removeChild(tempInput);
 
-  var tooltip = document.getElementById("myTooltip");
-tooltip.innerHTML = "Copied to clipboard!";
+
+
+
+// carousal
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-function outFunc() {
-  var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Click to copy mail";
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
-
-
-
-  function openModal() {
-    document.getElementById("myModal").style.display = "block";
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  } 
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  } 
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
-  
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+// loader
+var myVar;
+function myFunction() {
+  var quotes = [
+    "I'm an engineer turned designer 👩🏽‍🎓",
+    "My highest sleep record is 18 hrs 😴",
+    "I possess story telling super power 🪄",
+    "I ❤ dark coffee",
+    "Love at first sight ~ Bentley ❤",
+    "I can’t remain idle 💃",
+    "Roshni ~ Light ~ ⚡️",
+    "My current read ~ The Book Thief 📖",
+    "Mystery/Thriller is my fav 💀"
+  ]
+  var random = Math.floor(Math.random() * (quotes.length));
+  var quotenum = random + 1
+  document.getElementById("quotenum").innerHTML = quotenum + "";
+  document.getElementById("quote").innerHTML = quotes[random];
+  myVar = setTimeout(showPage, 1500);
 
-  var myVar;
-  function myFunction() {
-    var quotes = [
-      "I'm an engineer turned designer :0",
-      "My highest sleep record- 18 hrs😴",
-      "I possess story telling super power :D",
-      "I 🤎 dark coffee.",
-      "Love at first sight- Bentley ❤",
-      "I can’t remain idle 💃",
-      "Roshni ~ Light ~ ⚡️",
-      "My current read📖 Don't Make Me Think",
-      "Mystery/Thriller is my fav 📖"
-    ]
-    var random = Math.floor(Math.random() * (quotes.length));
-    var quotenum =random+1
-    document.getElementById("quotenum").innerHTML = quotenum + "";
-    document.getElementById("quote").innerHTML = quotes[random];
-    myVar = setTimeout(showPage, 1500);
+  var link = document.getElementById("link-rp");
+  link.setAttribute("href", index.html);
 
-    var link = document.getElementById("link-rp");
-    link.setAttribute("href", "https://roshnidesigns.github.io/portfolio/");
-
-  }
-  
-  function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("myDiv").style.display = "block";
-  }
+}
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
 
 
